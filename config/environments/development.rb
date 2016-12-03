9a0e64a42f86a3727982a13dbf9ae7bc9559cffd
HFYEH <sharefun010407@gmail.com>
2016-12-03 13:32:13 +0800

    add i18n and install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,22 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+  # Action mailer ------------------------------------------------------
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Use letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+
+  # Use gmail
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
